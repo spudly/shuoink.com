bce7b1cae185ce8d0c02f1af26ad3cb7c313c7f6
Stephen Sorensen <spudly@users.noreply.github.com>
2023-07-05 08:01:32 -0400

    add ice/shuoink links/logos

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,45 @@
 import {FC} from 'react';
 import Footer from './Footer';
+import IceLogo from './logos/IceLogo';
+import ShuoinkLogo from './logos/ShuoinkLogo';
 
 const App: FC = () => (
-  <div className="min-h-screen flex flex-col">
+  <div className="min-h-[100svh] flex flex-col">
     <header>
-      <h1 className="text-6xl text-center">
-        Stephen Sorensen's Personal Web Portal
-      </h1>
+      <h1 className="text-6xl text-center">Stephen Sorensen</h1>
     </header>
-    <main className="flex-1 flex flex-col justify-center items-center">
-      <img
-        src="/under construction.gif"
-        alt="under construction"
-        className="w-[90%] max-h-full"
-      />
-      <caption>Under Construction</caption>
+    <main className="flex-1 flex flex-col justify-center items-center gap-16">
+      <section>
+        <img
+          src="/under construction.gif"
+          alt="under construction"
+          className="w-[90%] max-h-full"
+        />
+      </section>
+
+      <section>
+        <p className="text-center pb-4">I work at:</p>
+        <ul className="flex gap-8 justify-around">
+          <li>
+            <a
+              href="https://www.ice.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <IceLogo height={50} />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.shuoink.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <ShuoinkLogo height={50} />
+            </a>
+          </li>
+        </ul>
+      </section>
     </main>
     <Footer />
   </div>
