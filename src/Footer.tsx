609d2e7faeb3ad3a2401cc1b3c431c2a5d5f3980
Stephen Sorensen <spudly@users.noreply.github.com>
2023-07-20 19:39:15 -0400

    scroll snap

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -12,7 +12,9 @@ const Footer: FC = () => {
   return (
     <footer className="h-screen w-full flex flex-col bg-white even:bg-gray-100 snap-start">
       <div className="flex-1 flex flex-col justify-center">
-        <p className="text-6xl text-center">That's it. That's all there is.</p>
+        <p className="text-6xl text-center">
+          That's it. That's all there is. There ain't no more.
+        </p>
         <p className="text-6xl text-center">¯\_(ツ)_/¯</p>
       </div>
 
